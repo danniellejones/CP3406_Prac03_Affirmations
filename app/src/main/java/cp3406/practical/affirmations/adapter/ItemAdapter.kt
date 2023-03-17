bce7e78f4e68155a091c3a8b5b2f74c059cd6453
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-03-18 00:14:49 +1000

    Update to use icon and material cards

--- app/src/main/java/cp3406/practical/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/cp3406/practical/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cp3406.practical.affirmations.R
@@ -12,23 +13,25 @@ import cp3406.practical.affirmations.model.Affirmation
 class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ItemViewHolder {
         val adapterLayout =
             LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
 
-        return ItemViewHolder(adapterLayout)
+        return ItemAdapter.ItemViewHolder(adapterLayout)
     }
 
     override fun getItemCount(): Int {
         return dataset.size
     }
 
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 }
