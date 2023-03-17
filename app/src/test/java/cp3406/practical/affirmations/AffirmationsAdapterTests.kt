665376030e35994c2272248670a725de013d0bfe
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-03-18 01:08:37 +1000

    Add tests

---
+++ app/src/test/java/cp3406/practical/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,23 @@
+package cp3406.practical.affirmations
+
+import android.content.Context
+import cp3406.practical.affirmations.adapter.ItemAdapter
+import cp3406.practical.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
