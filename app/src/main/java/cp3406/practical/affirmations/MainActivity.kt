e2982d149ffd7589e543550465d38a4e2c9ec869
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-03-17 21:53:26 +1000

    Update to use affirmations text views

--- app/src/main/java/cp3406/practical/affirmations/MainActivity.kt
+++ app/src/main/java/cp3406/practical/affirmations/MainActivity.kt
@@ -2,7 +2,8 @@ package cp3406.practical.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cp3406.practical.affirmations.adapter.ItemAdapter
 import cp3406.practical.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = Datasource().loadAffirmations().size.toString()
+        // Store returned list
+        val myDataset = Datasource().loadAffirmations()
+        // Look for view in layout and return reference to it
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        // Assign ItemAdapter object to the adapter property of recyclerView
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        // Size is fixed to improve performance - use if no change in layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+
     }
 }
